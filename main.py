9a174a95a4a703fc671749254aceeabc2e569054
Lachin Ashirova <60476768+Lashirova@users.noreply.github.com>
2021-11-02 23:30:25 +0000

    minor changes

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-def SubsetSum(array, no, val, sum):
+def SubsetSum(array, n, val, sum):
     if (sum == 0) :
       for value in val :
        print(value, end=" ")
        print()
        return
-    if (no == 0):
+    if (n == 0):
        return
-    SubsetSum(array, no - 1, val, sum)
+    SubsetSum(array, n - 1, val, sum)
     v1 = [] + val
-    v1.append(array[no - 1])
-    SubsetSum(array, no - 1, v1, sum - array[no - 1])
+    v1.append(array[n - 1])
+    SubsetSum(array, n - 1, v1, sum - array[n - 1])
 
-def isSubsets(array, no, sum):
+def isSubsets(array, n, sum):
     val = []
-    SubsetSum(array, no, val, sum)
+    SubsetSum(array, n, val, sum)
 array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 sum = 23
-no = len(array)
+n = len(array)
 
-isSubsets(array, no, sum)
+isSubsets(array, n, sum)
